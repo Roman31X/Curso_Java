a4b216c857cbd20b2248c10f2da521c96ec99dc7
Roman31X <roman.haa31@gmail.com>
2023-04-17 22:07:40 -0500

    j

---
+++ src/Condiciones/Ejer_Condicion.java
@@ -0,0 +1,19 @@
+package Condiciones;
+
+public class Ejer_Condicion {
+    public static void main(String[] args) {
+        /*
+         * Un usuario se logea en la plataforma y desea saver
+         * saber si lo logro o no
+         * */
+        //declarar una variable
+        boolean login = true;
+        //usaremos condicional
+        if(login){
+            System.out.println("Se logeo correctamente");
+        }else{
+            System.out.println("No se logeo correctamente");
+        }
+    }
+
+}
