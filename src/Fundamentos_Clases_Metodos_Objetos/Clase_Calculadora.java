5efab4a66b1bbc8831fa8e1c408a9cb273e1b9b2
Roman31X <die.haa65@gmail.com>
2023-05-22 14:32:21 -0500

    Curso_Java_Dia40.2

--- src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculadora.java
+++ src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculadora.java
@@ -3,24 +3,44 @@ package Fundamentos_Clases_Metodos_Objetos;
 import java.util.Scanner;
 
 public class Clase_Calculadora {
+    //DECLARAMOS VÁRIABLES GLOBALES
+    public static int a=0,b=0;
     public static void main(String[] args) {
         //IMPORTAMOS LA CLASE SCANNER
         Scanner sc = new Scanner(System.in);
         //creamos un objeto instaciado de la clase "Cálculo"
         Clase_Calculo cal;
-        //DECLARAMOS VÁRIABLES
-        int a,b;
         //Solicitamos datos para realizar calculo
         System.out.println("Ingrese valor para \"A\"");
         a = sc.nextInt();
         System.out.println("Ingrese valor para \"B\"");
         b = sc.nextInt();
-        //Realizamos calculo
+        //Realizamos calculo ENVIANDO LOS DATOS MEDINATE EL OBJETOS
+        //A LA CLASE QUE RECIVIRAN COMO PARAMETRO "a" y "b"
+        //COMO SE MOSTRAR EN EL SIGUIENTE EJEMPLO
         cal = new Clase_Calculo();
         System.out.println(cal.Valor(a,b));
         System.out.println("La suma es : "+cal.Suma(a,b));
         System.out.println("La resta es : "+cal.Resta(a,b));
         System.out.println("La multiplicación es : "+cal.Multiplicacion(a,b));
         System.out.println("La division es : "+cal.Division(a,b));
+        //mostramos los resultados de los métodos sin parametros
+        System.out.println("La suma de ["+a+"] + ["+b+"] es : ["+suma()+"]");
+        System.out.println("La resta de ["+a+"] - ["+b+"] es : ["+resta()+"]");
+        System.out.println("La multiplicación de ["+a+"] * ["+b+"] es : ["+multiplicar()+"]");
+        System.out.println("La division de ["+a+"] / ["+b+"] es : ["+division()+"]");
+    }
+    //METODOS DE LA CLASE SIN PARAMETROS
+    public static int suma(){
+        return (a+b);
+    }
+    public static int resta(){
+        return (a-b);
+    }
+    public static int multiplicar(){
+        return (a*b);
+    }
+    public static int division(){
+        return (a/b);
     }
 }
