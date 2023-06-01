80d1be86e9280cf550a4d812fdd4b3ae71a82fb7
Roman31X <die.haa65@gmail.com>
2023-05-31 19:30:08 -0500

    Curso_Java_Curso_Dia48.4

--- src/Argumentos_Variables/TestArgumentosVariables.java
+++ src/Argumentos_Variables/TestArgumentosVariables.java
@@ -3,7 +3,7 @@ package Argumentos_Variables;
 public class TestArgumentosVariables {
     public static void main(String[] args) {
         imprimirNumeros(2,4,6,8,10,12,14);
-        System.out.println("||========||");
+        System.out.println("||=======||");
         imprimirNumeros(1,3,5,7,9,11,13);
         System.out.println("||========||");
         variosParametros("Diego",15,18,20,19);
