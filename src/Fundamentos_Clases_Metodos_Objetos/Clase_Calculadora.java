88119e9059723920ecc1602c7329dcff4ee18a16
Roman31X <die.haa65@gmail.com>
2024-05-21 15:10:00 -0500

    Correciones

--- src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculadora.java
+++ src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculadora.java
@@ -7,11 +7,14 @@ public class Clase_Calculadora {
      * MEMORIA [STACK - HEAP]
      * MEMORIA STACK : almacena datos de variables locales
      * MEMORIA HEAP : almacena datos de variables tipo objetos*/
-    //DECLARAMOS VÁRIABLES GLOBALES
+
+    //DECLARAMOS VARIABLES GLOBALES
     public static int a=0,b=0;
+
     public static void main(String[] args) {
         //IMPORTAMOS LA CLASE SCANNER
         Scanner sc = new Scanner(System.in);
+
         //creamos un objeto instaciado de la clase "Cálculo"
         Clase_Calculo cal;
         //Solicitamos datos para realizar calculo
@@ -19,8 +22,9 @@ public class Clase_Calculadora {
         a = sc.nextInt();
         System.out.println("Ingrese valor para \"B\"");
         b = sc.nextInt();
-        //Realizamos calculo ENVIANDO LOS DATOS MEDINATE EL OBJETOS
-        //A LA CLASE QUE RECIVIRAN COMO PARAMETRO "a" y "b"
+
+        //Realizamos calculo ENVIANDO LOS DATOS MEDIANTE EL OBJETOS
+        //A LA CLASE QUE RECIBIRÁN COMO PARÁMETRO "a" y "b"
         //COMO SE MOSTRAR EN EL SIGUIENTE EJEMPLO
         cal = new Clase_Calculo();
         System.out.println(cal.Valor(a,b));
@@ -28,13 +32,15 @@ public class Clase_Calculadora {
         System.out.println("La resta es : "+cal.Resta(a,b));
         System.out.println("La multiplicación es : "+cal.Multiplicacion(a,b));
         System.out.println("La division es : "+cal.Division(a,b));
+
         //mostramos los resultados de los métodos sin parametros
         System.out.println("La suma de ["+a+"] + ["+b+"] es : ["+suma()+"]");
         System.out.println("La resta de ["+a+"] - ["+b+"] es : ["+resta()+"]");
         System.out.println("La multiplicación de ["+a+"] * ["+b+"] es : ["+multiplicar()+"]");
         System.out.println("La division de ["+a+"] / ["+b+"] es : ["+division()+"]");
     }
-    //METODOS DE LA CLASE SIN PARAMETROS
+
+    //MÉTODOS DE LA CLASE SIN PARÁMETROS
     public static int suma(){
         return (a+b);
     }
--- src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculo.java
+++ src/Fundamentos_Clases_Metodos_Objetos/Clase_Calculo.java
@@ -8,16 +8,18 @@ public class Clase_Calculo {
         System.out.println(Valor(a,b));
         System.out.println("La suma de a + b es: "+Suma(a,b));
         System.out.println("La resta de a - b es: "+Resta(a,b));
-        System.out.println("La multiplicación de a * b es: "+Multiplicacion(a,b));
+        System.out.println("La multiplicación de a * b es: "+ Multiplicación(a,b));
         System.out.println("La division de a '/' b es: "+Division(a,b));
     }*/
+
     //crear clases son buenas prácticas
     //creamos las clases fuera del main
     //método para mostrar datos de la variable
     //JAVADOC [describir la función de la clase]
     //usa /**Descripción*/
+
     /**DESCRIPCIÓN
-    * La clase Valor nos retornara una cadena de caracteres
+    * La clase Valor nos retorna una cadena de caracteres
     * la cual está siendo manipulada por la clase de tipo
     * @param a: Es el valor de la variable
     * @param b: Es el valor de la variable
@@ -28,11 +30,11 @@ public class Clase_Calculo {
     }
 
     /**
-     * Descripcion: esta función realiza la suma de los valores
+     * Descripción: esta función realiza la suma de los valores
      * asignados
      * @param a  Valor asignado a
      * @param b  Valor asignado b
-     * Finalmmente reguresa el valor de la suma
+     * Finalmente regresa el valor de la suma
      * */
 
     public int Suma(int a, int b){
