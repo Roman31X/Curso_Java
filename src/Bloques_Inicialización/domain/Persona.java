25221989f39754955f52398ca3f02b25050c0704
Roman31X <die.haa65@gmail.com>
2024-05-22 15:30:00 -0500

    modificaciones foreach

--- src/Bloques_Inicialización/domain/Persona.java
+++ src/Bloques_Inicialización/domain/Persona.java
@@ -3,6 +3,7 @@ package Bloques_Inicialización.domain;
 public class Persona {
     private final int idPersona;
     private static int contadorPresona;
+
     //este bloque se carga una sola vez en memoria al inicial la ejecución
     static{
         System.out.println("EJECUCIÓN BLOQUE ESTATICO");
@@ -10,15 +11,18 @@ public class Persona {
         ++Persona.contadorPresona;
         // no se puede incializar en estatico al no ser de ese tipo idPersona = 10;
     }
+
     //BLOQUE DE INICIALIZACIÓN ESTE SE EJECUTA ANTES DEL CONSTRUCTOR DE LA CLASE
     {
         System.out.println("EJECUCIÓN DE BLOQUE NO ESTATICO");
         this.idPersona = Persona.contadorPresona++;
     }
+
     //CONSTRUCTOR
     public Persona(){
         System.out.println("EJECUCIÓN DEL CONSTRUCTOR");
     }
+
     //RECUPERAR EL ID PERSONA
     public int getIdPersona() {
         return idPersona;
